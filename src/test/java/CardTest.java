7237ee3e50299265709b318b91f849bc8ac7b30c
Nikita Santalov <na.santalov@yandex.ru>
2022-09-03 22:11:18 +0400

    добавлен метод +3 дня для поля даты отредактирован тест

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -3,9 +3,11 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static java.time.Duration.ofSeconds;
 
 public class CardTest {
@@ -17,6 +19,8 @@ public class CardTest {
 
     @Test
     public void happyPathTest() {
+        String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $x("//input[@placeholder=\"Дата встречи\"]").val(meetingDate);
         SelenideElement form = $x("//form[contains(@class,form)]");
         SelenideElement notif = $x("//div[@data-test-id=\"notification\"]");
         $x("//input[@placeholder=\"Город\"]").val("Ижевск");
@@ -27,8 +31,7 @@ public class CardTest {
         form.$x(".//span[text()=\"Забронировать\"]//ancestor::button").click();
         notif.should(visible, ofSeconds(15));
         notif.$x(".//div[@class='notification__title']").should(text("Успешно!"));
-        notif.$x(".//div[@class='notification__content']").shouldHave(Condition.exactText("Встреча успешно забронирована на " + form.$x(".//span[@data-test-id='date']//input").getValue()));
-
+        $("[data-test-id=notification] .notification__content").should(exactText("Встреча успешно забронирована на " + meetingDate));
     }
 
 
